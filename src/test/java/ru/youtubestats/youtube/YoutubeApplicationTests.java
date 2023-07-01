9bb3b3af030c757035870d736db74dff882a2952
Anton <jimmy.page583@gmail.com>
2023-07-01 12:19:33 +0300

    initial commit

---
+++ src/test/java/ru/youtubestats/youtube/YoutubeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.youtubestats.youtube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
